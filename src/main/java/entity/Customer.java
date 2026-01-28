27776aed6ef11eb9a01c127fdd3188abf48b955a
Doom <de.simone.dc@gmail.com>
2026-01-28 15:54:46 +0100

    feat: add customer entity

---
+++ src/main/java/entity/Customer.java
@@ -0,0 +1,98 @@
+package entity;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private long id;
+
+    // private long storeId;
+
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+
+    //private Address addressId;
+
+    @Column(name = "active", nullable = false)
+    private char active;
+
+    @Column(name = "create_date", nullable = false)
+    private LocalDateTime createdDate;
+
+    @Column(name = "update_date", nullable = false)
+    private Timestamp updatedDate;
+
+    public Customer() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getActive() {
+        return active;
+    }
+
+    public void setActive(char active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public Timestamp getUpdatedDate() {
+        return updatedDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", active=" + active +
+                ", createdDate=" + createdDate +
+                ", updatedDate=" + updatedDate +
+                '}';
+    }
+}
