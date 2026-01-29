2a4a9aa20c0b9d4dde6e717b461427f676d311c7
Doom <de.simone.dc@gmail.com>
2026-01-29 14:41:05 +0100

    feat : add address entity

---
+++ src/main/java/entity/Address.java
@@ -0,0 +1,115 @@
+package entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "address",  nullable = false, length = 50)
+    private String address;
+
+    @Column(name = "address2",  nullable = true, length = 50)
+    private String address2;
+
+    @Column(name = "district",   nullable = false, length = 50)
+    private String district;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "city_id", nullable = false)
+    private City city;
+
+    @Column(name = "postal_code",  nullable = false, length = 10)
+    private String postalCode;
+
+    @Column(name = "phone", nullable = false, length = 20)
+    private String phone;
+
+    @Column(name = "last_update",  nullable = false)
+    private LocalDateTime lastUpdate;
+
+    public Address()
+    {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", address='" + address + '\'' +
+                ", address2='" + address2 + '\'' +
+                ", district='" + district + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", phone='" + phone + '\'' +
+                ", lastUpdate=" + lastUpdate +
+                '}';
+    }
+
+    @PrePersist
+    @PreUpdate
+    public void updateTimestamp() {
+        lastUpdate = LocalDateTime.now();
+    }
+}
